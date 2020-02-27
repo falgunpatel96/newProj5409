dc65b0b31b45288d9a2e3ca0b3f18c6d98152168
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-02-27 16:06:10 -0400

    Modified fibonacci prog from user2

--- fibonacci.py
+++ fibonacci.py
@@ -2,9 +2,11 @@ def fibo( no ):
     if no <= 0:
         raise Exception("Inout not valid...!!!");
     elif no == 1:
-        return 1;
+        return 0;
     elif no == 2:
         return 1;
     return fibo( no-1 ) + fibo( no-2 );
 
-print(fibo(6));
+f = open("E:\\2term\\Cloud\\Assignment2\\input.txt","r");
+for no in f:
+    print("Fibo of "+no+": "+str(fibo(int(no))));
