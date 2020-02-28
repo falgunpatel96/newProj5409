31a621bb8f8ea9f40e9307f391141e0bb07cbf58
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-02-27 20:47:46 -0400

    Efficient fibonacci prog from user2

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,19 @@
 def fibo( no ):
+    a = 0
+    b = 1
     if no <= 0:
-        raise Exception("Inout not valid...!!!");
-    elif no == 1:
-        return 0;
-    elif no == 2:
-        return 1;
-    return fibo( no-1 ) + fibo( no-2 );
+        raise Exception("Input not valid...!!!")
+    elif no == 1 or no == 2:
+        print(no-1, end=' ')
+    else:
+        for i in range(3,no):
+            c = a + b
+            a = b
+            b = c
+        print(b, end=' ')
 
-f = open("input.txt","r");
+f = open("input.txt","r")
 for no in f:
-    print("Fibo of "+no+": "+str(fibo(int(no))));
+    print("\n\nFibo of "+str(no).strip()+": ", end=' ')
+    for i in range(int(str(no).strip())):
+        fibo(int(i+1))
