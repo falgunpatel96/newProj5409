2a17bd43d2c8da555ccdb76be5293a3f471aaf6a
Falgun Dharmeshbhai Patel <fl889399@dal.ca>
2020-02-28 21:21:46 -0400

    with logfile for fact from user1

--- factorial.py
+++ factorial.py
@@ -1,3 +1,6 @@
+import time
+import json
+
 def fact( no ):
     if no < 0:
         raise Exception("Factorial of negative number is not possible...!!!");
@@ -6,7 +9,35 @@ def fact( no ):
     return no * fact(no-1);
 
 
-
+finalLog = [];
 f = open("input.txt","r");
-for no in f:
-    print("Fact of "+str(no).strip()+": "+str(fact(int(no))));
+for index,no in enumerate(f):
+    logNo = {};
+    start = time.time()
+    result = str(fact(int(no)));
+    end = time.time()
+    #print("Fact of "+str(no).strip()+": "+result);
+    #print("time: "+str(end-start))
+
+    # logNo.append(index+1);
+    # logNo.append(str(end-start));
+    # logNo.append(no.strip());
+    # logNo.append(result);
+
+    logNo['ID'] = index+1;
+    logNo['time'] = str(end-start);
+    logNo['no'] = no.strip();
+    logNo['result'] = result;
+
+    finalLog.append(logNo);
+    #print("Log: "+str(logNo));
+    #print("\n\n");
+
+#print("finalLog: "+str(finalLog));
+
+json_log = json.dumps(finalLog);
+#print("\n\n\n");
+print("JSON: "+json_log);
+with open("LogFibo.json",mode='w',newline="\n") as file:
+    file.write(json_log);
+file.close();
